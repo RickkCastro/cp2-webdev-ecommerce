53a26ef673876bd0206a2bb5cffe1d468767f0fc
rm564560@fiap.com.br <rm564560@fiap.com.br>
2025-10-16 20:57:04 -0300

    Add heading for featured products in Section component

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -6,7 +6,8 @@ export default function Section({ title, products }) {
       <h1 className="text-4xl font-bold text-center p-5 bg-amber-400 my-10">
         {title}
       </h1>
-      <div className="flex gap-4 flex-wrap">
+      <h3 className="text-2xl mx-5 mb-2">Produtos em destaque</h3>
+      <div className="flex gap-4 flex-wrap mx-5">
         {products != undefined &&
           products.map((product, index) => (
             <Card key={index} product={product} />
